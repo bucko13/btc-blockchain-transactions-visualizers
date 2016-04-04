6e90a258c98a9accf952116a60cbcb8423debfaa
Bucko <bucko.perley@gmail.com>
2016-04-04 15:08:50 -0700

    calling toshi websockets with angular factory

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-angular.module('bitcoin', [
-  'bitcoin.total'
+angular.module('bitcoinApp', [
+  'ngWebSocket'
 ])
+
+.factory('StreamTransactions', function($websocket) {
+  // Open a WebSocket connection 
+  var dataStream = $websocket('wss://bitcoin.toshi.io');
+
+  var collection = [];
+
+  dataStream.onMessage(function(message) {
+    collection.push(JSON.parse(message.data));
+  });
+
+  var methods = {
+    collection: collection,
+    get: function() {
+      dataStream.send(JSON.stringify({ subscribe: 'transactions' }));
+    }
+  };
+  return methods;
+})
+
+.controller('TransactionData', ['$scope', 'StreamTransactions', 
+  function($scope, StreamTransactions) {
+    // StreamTransactions.get()
+    $scope.transactions = StreamTransactions.collection;
+    // console.log(StreamTransactions.get())
+}])
