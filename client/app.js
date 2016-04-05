9bec90b6b54e07f3a8dd40d3d97070973354d54b
Bucko <bucko.perley@gmail.com>
2016-04-04 19:11:38 -0700

    code cleanup and adding a d3 module (still to build)

--- client/app.js
+++ client/app.js
@@ -1,41 +1,11 @@
 angular.module('bitcoinApp', [
-  'ngWebSocket'
+  'ngWebSocket',
+  'd3'
 ])
 
-.factory('StreamTransactions', function($websocket) {
-  // Open a WebSocket connection 
-  var dataStream = $websocket('wss://bitcoin.toshi.io');
-
-  var collection = [];
-  var totalArray = [];
-  var total = 0;
-
-  dataStream.onMessage(function(message) {
-    var amount = JSON.parse(message.data).data.amount
-    amount = parseFloat(amount > 0 ? (amount / 1e8).toFixed(8) : 0);
-    total = total + amount;
-    collection.push(amount);
-    totalArray.push(Math.round(total * 10000) / 10000);
-    // console.log(methods.total);      
-  });
-
-  dataStream.onClose(function(data) {
-    console.log('stream closed: ', data);
-  });
-
-  var methods = {
-    collection: collection,
-    totalArray: totalArray,
-    total: total,
-    get: function() {
-      dataStream.send(JSON.stringify({ subscribe: 'transactions' }));
-    },
-  };
-  return dataStream;
-})
-
-.controller('TransactionData', ['$scope', 'StreamTransactions', 
-  function($scope, StreamTransactions) {
+.controller('TransactionData', ['$scope', '$websocket', 
+  function($scope, $websocket) {
+    var StreamTransactions = $websocket('wss://bitcoin.toshi.io');
     var total = 0;
     var transactions = [];
     StreamTransactions.onMessage(function(message) {
@@ -48,17 +18,11 @@ angular.module('bitcoinApp', [
       $scope.transactions = transactions;
     });
     StreamTransactions.send(JSON.stringify({ subscribe: 'transactions' }));
-    
-    // StreamTransactions.get();
-    // var transactions = StreamTransactions.collection;
-    // var totalTransactions = StreamTransactions.totalArray;
-    // $scope.totalTransactions = totalTransactions;
 
     $scope.resetTransactions = function() {
       $scope.totalTransactions = 0;
       total = 0;
       transactions = [];
-      console.log(total);
       $scope.transactions = [];
 
     }
---
+++ client/lib/d3module.js
@@ -0,0 +1,6 @@
+angular.module('d3', [])
+  .factory('d3Service', [function(){
+    var d3;
+    // insert d3 code here
+    return d3;
+}]);
