0bb94a65e50b9df7477a17f1d2ce3f021f673487
Bucko <bucko.perley@gmail.com>
2016-04-05 15:56:05 -0700

    trying another update to server file for heroku deployment

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
+app.set('port', (process.env.PORT || 5000));
 
-server.listen(5000);
 app.use(express.static(__dirname + '/../client/'));
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
